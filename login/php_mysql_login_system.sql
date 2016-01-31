7eb5971485a0c3f4eff5511a78157e9c0dc4bb1f
Fernando Bueno <buenofernando13@gmail.com>
2016-01-31 10:14:12 -0200

    Create php_mysql_login_system.sql

---
+++ login/php_mysql_login_system.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
