9166aebbc99d4a077457eb36ed16fc721e1fb1e6
Fernando Bueno <buenofernando13@gmail.com>
2016-01-31 10:13:18 -0200

    Create db_const.php

---
+++ login/db_const.php
@@ -0,0 +1,7 @@
+<?php
+	# mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
+	const DB_HOST = 'mysql.dominio.com';
+	const DB_USER = 'usuario';
+	const DB_PASS = 'login';
+	const DB_NAME = 'phsql_login_system';
+?>
