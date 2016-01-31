370fed08bfe828c14504e7df0c27b9d942eb2730
Fernando Bueno <buenofernando13@gmail.com>
2016-01-31 10:06:46 -0200

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<title>User Login Form - PHP MySQL</title>
+</head>
+<body>
+<h1>User Login Form - PHP MySQL</h1>
+<?php
+if (!isset($_POST['submit'])){
+?>
+<!-- The HTML login form -->
+	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+		Username: <input type="text" name="username" /><br />
+		Password: <input type="password" name="password" /><br />
+ 
+		<input type="submit" name="submit" value="Login" />
+	</form>
+<?php
+} else {
+	require_once("db_const.php");
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	# check connection
+	if ($mysqli->connect_errno) {
+		echo "<p>MySQL error no {$mysqli->connect_errno} : {$mysqli->connect_error}</p>";
+		exit();
+	}
+ 
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+ 
+	$sql = "SELECT * from users WHERE username LIKE '{$username}' AND password LIKE '{$password}' LIMIT 1";
+	$result = $mysqli->query($sql);
+	if (!$result->num_rows == 1) {
+		echo "<p>Invalid username/password combination</p>";
+	} else {
+		echo "<p>Logged in successfully</p>";
+		// do stuffs
+	}
+}
+?>		
+</body>
+</html>
