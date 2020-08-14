bd8a4054fafa7f8693a328d92a766e156c3f4c60
Fernando Bueno <buenofernando13@gmail.com>
2020-08-14 03:47:07 -0300

    Update markting.php

--- email/markting.php
+++ email/markting.php
@@ -6,25 +6,7 @@ $subject = 'Birthday Reminders for August';
 
 // message
 $message = '
-<html>
-<head>
- <title>Birthday Reminders for August</title>
-</head>
-<body>
-<p>Here are the birthdays upcoming in August!</p>
-<table>
- <tr>
-  <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
- </tr>
- <tr>
-  <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
- </tr>
- <tr>
-  <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
- </tr>
-</table>
-</body>
-</html>
+html
 ';
 
 // To send HTML mail, the Content-type header must be set
