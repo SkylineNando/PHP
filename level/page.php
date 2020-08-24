5f888d99d30ae448201c920aa54c38d5608dec63
Fernando Bueno <buenofernando13@gmail.com>
2020-08-24 12:05:34 -0300

    Update page.php

--- level/page.php
+++ level/page.php
@@ -1,8 +1,17 @@
 <?php
 
-  // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
-  if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']))) {
+  // A sessão precisa ser iniciada em cada página diferente
+  if (!isset($_SESSION)) session_start();
+
+  // Verifica se não há a variável da sessão que identifica o usuário
+  if (!isset($_SESSION['UsuarioID'])) {
+      // Destrói a sessão por segurança
+      session_destroy();
+      // Redireciona o visitante de volta pro login
       header("Location: index.php"); exit;
   }
 
   ?>
+
+  <h1>Página restrita</h1>
+  <p>Olá, <?php echo $_SESSION['UsuarioNome']; ?>!</p>
